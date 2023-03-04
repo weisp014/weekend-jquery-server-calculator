fb5d8bc2637a19e3cad495cf5df0a33e90e97ff3
Ryan Weispfenning <weisp014@gmail.com>
2023-03-04 13:53:55 -0600

    completed computeResult function

--- server/Public/scripts/client.js
+++ server/Public/scripts/client.js
@@ -16,7 +16,7 @@ function getCalculation() {
         method: 'GET'
     })
     .then((response) => {
-        console.log('calculation data', response);
+        console.log('calculation history', response);
         render(response);
     }).catch((response) => {
         alert('request failed');
@@ -27,7 +27,7 @@ function render(response) {
     console.log('in render()', response);
     //empty history elements
     $('#historyHere').empty();
-    //TODO cycle through all objects and append to DOM
+    //cycle through all historical calculations and append to DOM
     for (let i=0; i < response.length; i++) {
         let result = "";
         result = response[i].num1 + response[i].operator + response[i].num2 + '=' + response[i].result
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8000;
 
 app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //array of all historical calculations
 let history = [
@@ -27,6 +27,38 @@ app.listen(PORT, () => {
     console.log('server running on port', PORT);
 });
 
+//TODO calculate and return result from incoming calculation
+function computeResult(equation) {
+    console.log('in computeResult()');
+    let result = 0;
+    let operation = equation.operator;
+    //check operator and calculate result
+    switch (operation) {
+        case '+':
+            console.log('addition detected');
+            result = (Number(equation.num1) + Number(equation.num2));
+            console.log(result);
+            break
+        case '-':
+            console.log('subtraction detected');
+            result = (Number(equation.num1) - Number(equation.num2));
+            console.log(result);
+            break
+        case '*':
+            console.log('multiplication detected');
+            result = (Number(equation.num1) * Number(equation.num2));
+            console.log(result);
+            break
+        case '/':
+            console.log('division detected');
+            result = (Number(equation.num1) / Number(equation.num2));
+            console.log(result);
+            break
+        default:
+            console.log('Error: unexpected operator');
+    }
+}
+
 //routes
 
 //GET
@@ -37,3 +69,18 @@ app.get('/calculation', (req, res) => {
 });
 
 //TODO POST
+app.post('/calculation', (req, res) => {
+    console.log('Get a POST request', req.body);
+
+    //grab new calculation from request body
+    let newCalculation = req.body;
+
+    //calculate result and add as new property
+    newCalculation.result = computeResult(newCalculation);
+
+    //add to the history array
+    history.push(newCalculation);
+
+    //send back status code
+    res.sendStatus(201);
+})
